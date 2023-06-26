aea2096722dc92a45b4a644ce79249c1a6ae564e
Briankim254 <kimutaibrian560@gmail.com>
2023-06-26 16:52:56 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'dark': {'raw': '(prefers-color-scheme: dark)'},
+      },
+      colors: {
+        darkMode: {
+          bg: '#1f2937',
+          text: '#f9fafb',
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['dark'],
+      textColor: ['dark'],
+    },
+  },
+  plugins: [],
+}
