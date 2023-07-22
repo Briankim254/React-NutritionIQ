585b6c8be8da330eaf6440dcf15e1c040c5ab680
Briankim254 <kimutaibrian560@gmail.com>
2023-07-23 02:08:14 +0300

    removed gooogle auth

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="570081466222-opn4ih3mp73aru9hirajdb3pthrsg15d.apps.googleusercontent.com">
     <React.StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </React.StrictMode>
-  </GoogleOAuthProvider>
 );
