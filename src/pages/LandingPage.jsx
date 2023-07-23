125ccfc20639934bd22d510e5f7e80ad6749bfc9
Briankim254 <kimutaibrian560@gmail.com>
2023-07-23 19:05:27 +0300

    sign up button

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -12,7 +12,7 @@ const LandingPage = () => {
         <div className="flex flex-col max-w-screen-lg overflow-hidden bg-slate-800 rounded shadow-sm lg:flex-row sm:mx-auto">
           <div className="relative lg:w-1/2">
             <img
-              src="/sweet-life-OxkxluMeAGs-unsplash.jpg"
+              src="https://images.unsplash.com/photo-1683537277115-502a6eecb6ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
               alt=""
               className="object-cover w-full lg:absolute h-80 lg:h-full"
             />
