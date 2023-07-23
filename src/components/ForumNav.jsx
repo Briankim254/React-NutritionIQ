fbb5ccfe268d751a05eade11b2adc36881d1ab0e
Briankim254 <kimutaibrian560@gmail.com>
2023-07-23 12:34:30 +0300

    implimented user authentication and management with @clerk

--- src/components/ForumNav.jsx
+++ src/components/ForumNav.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import Logo from "../components/logo";
+import { NavLink } from "react-router-dom";
+import { UserButton, SignedIn, SignInButton, SignedOut } from "@clerk/clerk-react";
+
 const ForumNavigation = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -8,11 +11,14 @@ const ForumNavigation = () => {
   };
 
   return (
-    <nav className="sticky backdrop-blur top-0 z-10 rounded-xl  shadow " x-data="{ isOpen: false }">
+    <nav
+      className="sticky backdrop-blur top-0 z-10 rounded-xl  shadow "
+      x-data="{ isOpen: false }"
+    >
       <div className="container px-6 py-4 mx-auto">
         <div className="lg:flex lg:items-center lg:justify-between">
           <div className="flex items-center justify-between">
-            <Logo/>
+            <Logo />
 
             <div className="flex lg:hidden">
               <button
@@ -88,45 +94,25 @@ const ForumNavigation = () => {
               >
                 Experts
               </a>
-            </div>
-
-            <div className="flex items-center mt-4 lg:mt-0">
-              <button
-                className="hidden mx-4 text-gray-600 transition-colors duration-300 transform lg:block  hover:text-gray-700  focus:text-gray-700  focus:outline-none"
-                aria-label="show notifications"
+              <SignedIn>
+              <NavLink
+                className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0  hover:bg-gray-100 "
+                to="/profile"
               >
-                <svg
-                  className="w-6 h-6"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </button>
-
-              <button
-                type="button"
-                className="flex items-center focus:outline-none"
-                aria-label="toggle profile dropdown"
-              >
-                <div className="w-8 h-8 overflow-hidden border-2 border-gray-400 rounded-full">
-                  <img
-                    src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
-                    className="object-cover w-full h-full"
-                    alt="avatar"
-                  />
-                </div>
-
-                <h3 className="mx-2 text-gray-700  lg:hidden">Khatab wedaa</h3>
-              </button>
+                Profile
+              </NavLink>
+            </SignedIn>
             </div>
+            
+            <SignedOut>
+              <NavLink
+                to="/forum"
+                className="px-4 py-2 mt-2 text-sm font-medium leading-5 text-center text-white capitalize bg-blue-800 rounded-lg hover:bg-blue-700 lg:mx-0 lg:w-auto focus:outline-none"
+              >
+                Sign In
+              </NavLink>
+            </SignedOut>
+            <UserButton />
           </div>
         </div>
       </div>
